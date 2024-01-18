99aaca47b4f20994bf233beef38b91a4c20ebd38
salehmb1999 <salehmirbagheri78@gmail.com>
2024-01-18 11:29:11 +0330

    complete profile section without responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,35 @@
 const hamburgerMenuLine = document.querySelector(".hamburgerMenuLine");
-const mobileNavMenu = document.querySelector(".mobileNavMenu")
+const mobileNavMenu = document.querySelector(".mobileNavMenu");
+const profileBtn = document.querySelector(".profileBtn");
+const profileBtn2 = document.querySelector(".profileBtn2");
+const linkedinIcon = document.querySelector(".linkedinIcon");
+const githubIcon = document.querySelector(".githubIcon");
 let menuOpen = false;
 hamburgerMenuLine.addEventListener("click", () => {
   if (!menuOpen) {
     hamburgerMenuLine.style.backgroundColor = "transparent";
-    mobileNavMenu.style.display = "flex"
+    mobileNavMenu.style.display = "flex";
     hamburgerMenuLine.classList.add("open");
 
     menuOpen = true;
   } else {
     hamburgerMenuLine.style.backgroundColor = "#000";
     hamburgerMenuLine.classList.remove("open");
-    mobileNavMenu.style.display = "none"
+    mobileNavMenu.style.display = "none";
 
     menuOpen = false;
   }
+});
+profileBtn.addEventListener("click", () => {
+  window.open("./../assets/images/resume-example.pdf");
+});
+profileBtn2.addEventListener("click", () => {
+  location.href = "./#contact";
+});
+linkedinIcon.addEventListener("click", () => {
+  location.href =
+    "https://www.linkedin.com/in/seyed-saleh-mirbagheri-426882233/";
+});
+githubIcon.addEventListener("click", () => {
+  location.href = "https://github.com/salehMb1999";
 });
